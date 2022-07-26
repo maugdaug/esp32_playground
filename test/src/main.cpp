6f863b06b372d0377845ebd3afe0df9b0a78e80e
maugdaug <amauger87@gmail.com>
2022-07-26 08:35:12 +0000

    working wifi stuff on esp32

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,11 +1,38 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
 
 void setup() {
 	Serial.begin(115200);
+	WiFi.mode(WIFI_STA);
+	WiFi.disconnect();
+	delay(100);
 
+	Serial.print("Setup complete");
 }
 
 void loop() {
-	Serial.print("ping");
-	delay(1000);
+	Serial.println("WiFi scan starting...");
+	int n = WiFi.scanNetworks();
+	Serial.println("Scan complete!");
+	if (n==0) {
+		Serial.println("No networks found :(");
+	} else {
+		Serial.print("Networks found: ");
+		Serial.println(n);
+		
+		for (int i=0; i<n; i++) {
+			Serial.print(i+1);
+			Serial.print(": ");
+			Serial.print(WiFi.SSID(i));
+			Serial.print(" (");
+			//Serial.print(WiFi.RSSI);
+			Serial.print(")");
+			Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"+");
+			delay(10);
+		}
+	}
+
+	Serial.println();
+	delay(5000);
 }
