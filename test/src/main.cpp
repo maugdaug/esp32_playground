379bc23162461bea327c8bc2ebcaecba2933b34c
maugdaug <amauger87@gmail.com>
2022-08-09 07:49:19 +0000

    ESP32 connects to wifi and prints status/IP

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -2,16 +2,13 @@
 #include <WiFi.h>
 
 
-void setup() {
-	Serial.begin(115200);
-	WiFi.mode(WIFI_STA);
-	WiFi.disconnect();
-	delay(100);
+// WIFI CREDENTIALS
+const char* ssid = "interwebs";
+const char* password = "trust_the_rob0ts";
 
-	Serial.print("Setup complete");
-}
+int index = 0;
 
-void loop() {
+void scan_wifi() {	
 	Serial.println("WiFi scan starting...");
 	int n = WiFi.scanNetworks();
 	Serial.println("Scan complete!");
@@ -34,5 +31,67 @@ void loop() {
 	}
 
 	Serial.println();
-	delay(5000);
+}
+
+
+void connect_to_wifi() {
+
+	Serial.print("Connecting to WiFi...");
+	WiFi.begin(ssid, password);
+	while (WiFi.status() != WL_CONNECTED) {
+		Serial.print('.');
+		delay(500);
+	}
+}
+
+
+void report_wifi_status() {
+	
+	Serial.print(WiFi.status());
+	Serial.print("\tRSSI: ");
+	Serial.print(WiFi.RSSI());
+	Serial.print("\tIP: ");
+	Serial.print(WiFi.localIP());
+
+	Serial.println();
+}
+
+
+void set_static_IP() {
+	IPAddress local_IP(10,0,0,123);
+	IPAddress gateway(10,0,0,1);
+	IPAddress subnet(255,255,255,0);
+	IPAddress primaryDNS(75,75,75,75);
+	IPAddress secondaryDNS(75,75,76,76);
+
+	if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+		Serial.println("STA failed to configure!");
+	}
+}
+
+
+void setup() {
+	Serial.begin(115200);
+	WiFi.mode(WIFI_STA);
+	WiFi.disconnect();
+	delay(100);
+
+	//delay(10000);
+	connect_to_wifi();
+
+	delay(1000);
+
+	set_static_IP();
+}
+
+void loop() {
+	//scan_wifi();
+	delay(1000);
+
+	Serial.print(index);
+	Serial.print("\t");
+	Serial.print(millis());
+	Serial.print("     ");
+	report_wifi_status();
+
 }
